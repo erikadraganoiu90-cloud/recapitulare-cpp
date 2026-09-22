df9ce2ef73413dafd543d2b6acaada91185259d1
Erika <erikadraganoiu90@gmail.com>
2026-09-22 19:06:01 +0300

    finished   ex1

--- recapitulare/recapitulare.cpp
+++ recapitulare/recapitulare.cpp
@@ -1,4 +1,5 @@
 #include "lucrul-cifre.h"
+#include "rezolvare-fisa-2.h"
 
 int main()
 {
@@ -13,7 +14,81 @@ int main()
 	//int s1 = ciframaxima(6574);
 	//cout << s1;
 	//ex2();
-	int s1=eliminareaCifreiDeRangk(8459,2);
+	//int s1=eliminareaCifreiDeRangk(8459,2);
+	//cout << s1;
+
+	//TEMA
+	
+	//Ex1
+	//int s1=primaCifra(2346);
+	//cout<<s1;
+	
+	//Ex 2
+	//int s1=sumaCifrelor(4256);
+	//cout<<s1;
+	
+	//Ex 3
+	//int s1=produsulCifrelor(94267);
+	//cout<<s1;
+	
+	//Ex 4
+	//int s1=sumaCifrelorPare(92435);
+	//cout<<s1;
+	
+	//Ex5
+	//int s1=apareCifra(42754,5);
+	//cout<<s1;
+	 
+	//Ex6
+	//int s1=contorAparitie(1426,1);
+	//cout<<s1;
+	 
+	//Ex 7
+	//int s1=pozitieCifra(24536,3);
+	//cout<<s1;
+	
+	//Ex 8
+	//int s1=rasturnat(342567);
+	//cout<<s1;
+	
+	//Ex 9
+	//bool s1=palindrom(12321);
+	//cout<<s1;
+	
+	//Ex 10
+	//bool s1=repetareCifra(13242);
+	//cout<<s1;
+	
+	//Ex 11
+	//bool s1= cifCrescatoare(12545);
+	//cout << s1;
+
+	//Ex 12
+	//int s1 = alipire(123, 45);
+	//cout << s1;
+
+	//Ex 13
+	//int s1 = sumaPozImpr(54267);
+	//cout << s1;
+
+	//Ex 14
+	//int s1 = alipirePrimaUltima(7);
+	//cout << s1;
+
+	//Ex 15
+	//int s1=ctCifDif(4438);
+	//cout << s1;
+
+	//Ex 16
+	//int s1 = radacinaDigitala(9875);
+	//cout << s1;
+
+	//Ex17
+	//int s1=faraCifrePare(2345);
+	//cout << s1;
+
+	//Ex18
+	int s1 = cifreComune(4738, 8321);
 	cout << s1;
 }
 
---
+++ recapitulare/rezolvare-fisa-2.h
@@ -0,0 +1,286 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+//Ex 1
+int primaCifra(int n) { 
+	while (n > 10) {
+		 n = n / 10;
+	}
+	return n;
+}
+
+//Ex 2
+int sumaCifrelor(int n) {
+	int S = 0;
+	while (n > 0) {
+		int cif = n % 10;
+		S = S + cif;
+		n = n / 10;
+	}
+	return S;
+}
+
+//Ex 3
+int produsulCifrelor(int n) {
+	int P = 1;
+	while (n > 0) {
+		int cif = n % 10;
+		P = P * cif;
+		n = n / 10;
+	}
+	return P;
+}
+
+//Ex 4
+int sumaCifrelorPare(int n) {
+	int S = 0;
+	while (n > 0) {
+		int cif = n % 10;
+		if (cif % 2 == 0) {
+           S = S + cif;
+		}
+		n = n / 10;
+	}
+	return S;
+}
+
+//Ex5
+bool apareCifra(int n,int x) {
+	 
+	while (n > 0) {
+		int cif = n % 10;
+		if (cif != x) {
+			return false;
+		}
+		n = n / 10;
+	}
+	return true;
+}
+
+//Ex 6
+int contorAparitie(int n,int x) {
+	int ct = 0;
+	while (n > 0) {
+		int cif = n % 10;
+		if (cif == x) {
+			ct++;
+		}
+		n = n / 10;
+	}
+	return ct;
+}
+
+//Ex 7
+int pozitieCifra(int n, int x) {
+	 int ct = 0;
+	while (n > 0) {
+		int cif = n % 10;
+		ct++;
+		if (ct == x) {
+			return cif;
+		}
+		n = n / 10;
+	}
+	if (ct < x) {
+		int a = -1;
+		return a;
+	}
+}
+
+//Ex 8
+int rasturnat(int n) {
+	int r = 0;
+	while (n > 0) {
+		int cif = n % 10;
+		r = r * 10 + cif;
+		 n = n / 10;
+	}
+	
+	return r;
+}
+
+//Ex 9
+bool palindrom(int n) {
+	int ogl = 0;
+	int x = n;
+	while (n > 0) {
+		int cif = n % 10;
+		ogl = ogl * 10 + cif;
+		n = n / 10;
+	}
+	if (ogl == x) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+//Ex 10
+bool repetareCifra(int n) {
+	 
+	int ct[10] = { 0 };
+	while (n > 0) {
+		int cif = n % 10;
+		ct[cif]++;
+		n = n / 10;
+		if (ct[cif] != 1) {
+			 return false;
+		}
+	}return true;
+	 
+		 
+}
+
+// Ex 11
+bool cifCrescatoare(int n) {
+	while (n > 0) {
+		int cif = n % 10;
+		n = n / 10;
+		if (cif < n % 10) {
+			return false;
+		}
+		
+	}
+	return true;
+ }
+
+//Ex 12
+int alipire(int n, int m) {
+	int p = 1;
+	int m2 = m;
+	while (m > 0) {
+		m = m / 10;
+		p = p * 10;
+	}
+	int x = n * p + m2;
+	return x;
+}
+
+//Ex 13
+int sumaPozImpr(int n) {
+	int s = 0;
+	int p = 1;
+	while (n > 0) {
+		int cif = n % 10;
+		if (p % 2 != 0) {
+			s = s + cif;
+		}
+		n = n / 10;
+		p++;
+	}
+	return s;
+}
+
+//Ex 14
+int alipirePrimaUltima(int n) {
+	int uc = n % 10;
+	while (n > 10) {
+		n = n / 10;
+	}
+	int x = n * 10 + uc;
+	return x;
+}
+
+//Ex 15
+int ctCifDif(int n) {
+	int ct[10] = { 0 };
+	int ctc = 0;
+	while (n > 0) {
+		int cif = n % 10;
+		ct[cif]++;
+		n = n / 10;
+	}
+	int i = 1;
+	while (i <= 9) {
+		
+		if (ct[i] != 0) {
+			ctc++;
+		}i++;
+		}
+
+	return ctc;
+}
+
+//Ex 16
+int radacinaDigitala(int n) {
+	int s = 0;
+	while (n > 10) {
+       while (n > 0) {
+		int cif = n % 10;
+		s = s + cif;
+		n = n / 10;
+	   }
+     n = s;
+	}
+	if (n < 10) {
+		return n;
+	}
+        return s;
+	  
+}
+
+//Ex 17
+int faraCifrePare(int n) {
+	int p = 1;
+	int x = 0;
+	while (n > 0) {
+		int cif = n % 10;
+		if (cif % 2 != 0) {
+			 x = cif * p + x;
+			p = p * 10;
+		}
+		n = n / 10;
+	}
+	return x;
+}
+
+//Ex18
+int cifreComune(int n, int m) {
+	int ctn[10] = { 0 };
+	int ctm[10] = { 0 };
+	int cif;
+	while (n > 0) {
+		 cif = n % 10;
+		ctn[cif]++;
+		n = n / 10;
+	}
+	while (m > 0) {
+		 cif = m % 10;
+		ctm[cif]++;
+			m = m / 10;
+	}
+	int i = 1;
+	int ct = 0;
+	while (i <= 10) {
+		if (ctn[i] > 0 && ctm[i] > 0) {
+			ct++;
+		}
+		i++;
+	}
+	return ct;
+}
+
+//Ex19
+int nrMax(int n) {
+	int max = 0;
+	int i = 0;
+	int x = n;
+	while (x > 0) {
+		i++;
+		x = x / 10;
+	}
+	while (i > 0) {
+        while (n > 0) {
+		int cif = n % 10;
+		if (cif >= max) {
+			max[i] = cif;
+		}
+		 n = n / 10;
+	     }
+		x=max*i+
+        i--;
+	}
+	
+}
